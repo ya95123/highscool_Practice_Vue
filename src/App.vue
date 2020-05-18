52a8eca0789b5e2d7a9e15c40516f46a4c32b87e
ya95123 <ya95123@gmail.com>
2020-05-18 15:46:00 +0800

    right

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,31 @@
       Menu
       Update
     #right
+      Marquee
+      #content
+        Main(v-if="page === 'main'")
 </template>
 
 <script>
 import Top from './components/Top'
 import Menu from './components/Menu'
 import Update from './components/Update'
+import Marquee from './components/Marquee'
+import Main from './components/Main'
 
 export default {
   name: 'App',
   components: {
     Top,
     Menu,
-    Update
+    Update,
+    Marquee,
+    Main
+  },
+  data () {
+    return {
+      page: 'main'
+    }
   }
 }
 </script>
