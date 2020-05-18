7bb0f442788ccbffeb1e1e72ccaf0684c502187b
ya95123 <ya95123@gmail.com>
2020-05-18 15:13:20 +0800

    left 結束

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     Top
     #left
       Menu
+      Update
     #right
 </template>
 
 <script>
 import Top from './components/Top'
 import Menu from './components/Menu'
+import Update from './components/Update'
 
 export default {
   name: 'App',
   components: {
     Top,
-    Menu
+    Menu,
+    Update
   }
 }
 </script>
---
+++ src/components/Update.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  #update
+    | 最近更新日期:2020.05.18
+</template>
+
+<script>
+export default {
+  name: 'Update'
+}
+</script>
